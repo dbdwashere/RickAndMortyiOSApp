1c9fd3e57e316b76a828a02d17dc5280b22e8edd
dbdwashere <damlabusedev@gmail.com>
2025-02-08 15:38:54 +0300

    Add models

---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Damla Buse Demir on 7.02.2025.
+//
+
+import Foundation
+struct RMSingleLocation: Codable {
+      let name: String
+      let url: String
+
+}
