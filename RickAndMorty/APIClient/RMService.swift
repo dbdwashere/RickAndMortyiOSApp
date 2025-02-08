114c2258590250877582f034ba30b9d561f5e28a
dbdwashere <damlabusedev@gmail.com>
2025-02-08 18:25:57 +0300

    Create API Request

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -16,11 +16,16 @@ final class RMService{
     private init() {}
     
     ///Send Rick and Morty API Call
-    ///-Parameters:
-    ///- request: Request instance
-    ///- completion: Callback with data or error
+    /// - Parameters:
+    /// - request:Request instance
+    /// - type: The type of object we expect to get back
+    /// - completion: Callback with data or error
     
-    public func execute(_ request: RMRequest, completion: @escaping() -> Void) {
+    public func execute <T: Codable> (
+        _ request: RMRequest,
+        expecting type:T.Type,
+        completion: @escaping(Result<T, Error>) -> Void
+    ) {
         
     }
 }
